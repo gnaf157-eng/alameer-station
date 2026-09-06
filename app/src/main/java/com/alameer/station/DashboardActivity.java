acb45edcfe626ca0a2721442ac80020e67940f61
gnaf157-eng <gnaf.157@gmail.com>
2026-09-06 16:47:36 +0300

    Set unique shifts package in app/src/main/java/com/alameer/station/DashboardActivity.java

--- app/src/main/java/com/alameer/station/DashboardActivity.java
+++ app/src/main/java/com/alameer/station/DashboardActivity.java
@@ -1,4 +1,4 @@
-package com.alameer.station;
+package com.alameer.station.shifts;
 
 import android.app.*;import android.os.*;import android.content.*;import android.graphics.Color;import android.view.*;import android.widget.*;import java.text.*;import java.util.*;
 
